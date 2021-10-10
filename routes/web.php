4dee006f73648fef1eae24c7b44ebae25a23e902
bbana81 <bbana81@localhost.localdomain>
2021-10-10 19:15:18 +0900

    20211010 5-2 2nd edit

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ Route::get('/', function () {
 });
 
 //Route::get('hello', 'App\Http\Controllers\HelloController');
+Route::get('hello/add', 'App\Http\Controllers\HelloController@add');
+Route::post('hello/add', 'App\Http\Controllers\HelloController@create');
+Route::get('hello/edit', 'App\Http\Controllers\HelloController@edit');
+Route::post('hello/edit', 'App\Http\Controllers\HelloController@update');
+Route::get('hello/del', 'App\Http\Controllers\HelloController@del');
+Route::post('hello/del', 'App\Http\Controllers\HelloController@remove');
 Route::get('hello', 'App\Http\Controllers\HelloController@index');
 Route::post('hello', 'App\Http\Controllers\HelloController@post');
 //Route::get('hello/other', 'App\Http\Controllers\HelloController@other');
